50bbdf0f5c76a8c18dfcf993c5c9459847308673
Simon Benedikt Ritter <simon.b.ritter@gmail.com>
2016-08-03 17:16:34 +0200

    Repair loan and list loan functions

--- hbz-deliver-module/src/main/java/hbz/MainVerticle.java
+++ hbz-deliver-module/src/main/java/hbz/MainVerticle.java
@@ -71,6 +71,9 @@ public class MainVerticle extends AbstractVerticle {
 		router.post("/deliver/renew").handler(this::renew);
 		router.get("/deliver/loans/:patronId").handler(this::getLoansForPatron);
 		router.get("/deliver/listLoans").handler(this::showLoanListScreen);
+		router.get("/deliver/sampleData").handler(this::showSampleDataScreen);
+		router.post("/deliver/createPatron").handler(this::createPatron);
+		router.post("/deliver/createItem").handler(this::createItem);
 		vertx.createHttpServer().requestHandler(router::accept).listen(port, result -> {
 			if (result.succeeded()) {
 				fut.complete();
@@ -341,5 +344,72 @@ public class MainVerticle extends AbstractVerticle {
 			}
 		}).putHeader("content-type", "application/json").putHeader("accept", "text/plain")
 				.putHeader("authorization", authorization).putHeader("X-Okapi-Tenant", tenant).end(loanAsJson);
+	}
+	
+	// Sample Data methods
+	
+	private void showSampleDataScreen(RoutingContext routingContext) {
+		HttpClient httpClient = vertx.createHttpClient();
+		httpClient.get(9130, "localhost", "/apis/patrons/", response -> response.bodyHandler(buffer -> {
+			try {
+				JSONObject bufferAsJson = new JSONObject(buffer.toString());
+				JSONArray patrons = bufferAsJson.getJSONArray("patrons");
+				routingContext.put("patrons", patrons.toString(2));
+
+				httpClient
+						.get(9130, "localhost", "/apis/items/", itemResponse -> itemResponse.bodyHandler(itemBuffer -> {
+							try {
+								JSONObject itemBufferAsJson = new JSONObject(itemBuffer.toString());
+								JSONArray items = itemBufferAsJson.getJSONArray("items");
+								routingContext.put("items", items.toString(2));
+
+								engine.render(routingContext, "templates/sampleData.html",
+										engineResponse -> routingContext.response().setStatusCode(200)
+												.putHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+												.end(engineResponse.result()));
+
+							} catch (Exception e) {
+								logger.error(e);
+							}
+						})).putHeader("content-type", "application/json").putHeader("accept", "application/json")
+						.putHeader("authorization", authorization).putHeader("X-Okapi-Tenant", tenant).end();
+
+			} catch (Exception e) {
+				logger.error(e);
+			}
+		})).putHeader("content-type", "application/json").putHeader("accept", "application/json")
+				.putHeader("authorization", authorization).putHeader("X-Okapi-Tenant", tenant).end();
+	}
+
+	private void createPatron(RoutingContext routingContext) {
+		String patron = routingContext.getBodyAsString();
+		HttpClient httpClient = vertx.createHttpClient();
+		httpClient.post(9130, "localhost", "/apis/patrons/", response -> {
+			if (response.statusCode() == 201) {
+				routingContext.response().setStatusCode(201).putHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+						.end("Patron created");
+			} else {
+				routingContext.response().setStatusCode(500).putHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+						.end("Error creating patron");
+			}
+		}).putHeader("content-type", "application/json").putHeader("accept", "text/plain")
+				.putHeader("authorization", authorization).putHeader("X-Okapi-Tenant", tenant).end(patron);
+
+	}
+	
+	private void createItem(RoutingContext routingContext) {
+		String patron = routingContext.getBodyAsString();
+		HttpClient httpClient = vertx.createHttpClient();
+		httpClient.post(9130, "localhost", "/apis/items/", response -> {
+			if (response.statusCode() == 201) {
+				routingContext.response().setStatusCode(201).putHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+						.end("Item created");
+			} else {
+				routingContext.response().setStatusCode(500).putHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+						.end("Error creating item");
+			}
+		}).putHeader("content-type", "application/json").putHeader("accept", "text/plain")
+				.putHeader("authorization", authorization).putHeader("X-Okapi-Tenant", tenant).end(patron);
+
 	}
 }
